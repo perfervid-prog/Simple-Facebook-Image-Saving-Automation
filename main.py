ad89b4c0caf70f583732fef79cbaf99992c0eb3d
perfervid-prog <albertprem123@gmail.com>
2025-01-22 12:52:15 +0545

    Added running script

---
+++ main.py
@@ -0,0 +1,10 @@
+from fb_post_image_saver import FbPostImageSaver
+import time
+
+if __name__ == "__main__":
+    total_image = int(input("Enter the number of images you want to save: "))
+    file_name = int(input("Starting file name (only number), default is 1: "))
+    print("10 seconds left. Move the mouse cursor to the center of the image.")
+    time.sleep(10) # Adjust as needed
+    saver = FbPostImageSaver(total_image, file_name)
+    saver.run()
