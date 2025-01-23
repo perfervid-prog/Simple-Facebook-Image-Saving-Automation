d7e45eba8a8cbc03eb0d790e751ed1022ea93b62
perfervid-prog <albertprem123@gmail.com>
2025-01-23 07:48:19 +0545

    Solve file name writing

--- fb_post_image_saver.py
+++ fb_post_image_saver.py
@@ -11,12 +11,11 @@ class FbPostImageSaver:
         for i in range(1, self.total_image + 1):
             x, y = pyautogui.position() #gets the cursor position
             pyautogui.rightClick(x, y) #right click
-            time.sleep(1)
             pyautogui.moveTo(x + 20, y + 60) # move the cursor down to click on save as image option
             pyautogui.click()
             time.sleep(1)
-            pyautogui.write(str(file_name)) # write the file name i
-            pyautogui.moveTo(x - 20, y - 1) # again move the cursor up 
+            pyautogui.write(str(file_name) if i == 1 else file_name + i - 1) # write the file name i
+            pyautogui.moveTo(x-2, y - 1) # again move the cursor up 
             pyautogui.press('enter') # press enter
             time.sleep(1)
             pyautogui.press('right') # move to next image
