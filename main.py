417e3f3eff06c12898c6f776e39e9fae47a39f98
perfervid-prog <albertprem123@gmail.com>
2025-01-23 07:48:54 +0545

    Change the time delay to 5 seconds

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import time
 if __name__ == "__main__":
     total_image = int(input("Enter the number of images you want to save: "))
     file_name = int(input("Starting file name (only number), default is 1: "))
-    print("10 seconds left. Move the mouse cursor to the center of the image.")
-    time.sleep(10) # Adjust as needed
+    print("5 seconds left. Move the mouse cursor to the center of the image.")
+    time.sleep(5) # Adjust as needed
     saver = FbPostImageSaver(total_image, file_name)
     saver.run()
